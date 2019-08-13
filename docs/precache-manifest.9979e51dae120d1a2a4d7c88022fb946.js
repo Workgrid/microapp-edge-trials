4d3197bcb3319df4f8d410b9c7d8dc088c9e2157
Chris Weed <chris@workgrid.com>
2019-08-13 11:16:30 -0500

    🔥

---
+++ docs/precache-manifest.9979e51dae120d1a2a4d7c88022fb946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc86759b5a218e6e21848537e64ac36",
+    "url": "/microapp-edge-trials/index.html"
+  },
+  {
+    "revision": "455aff0743f0b953176b",
+    "url": "/microapp-edge-trials/static/css/main.e6d01b73.chunk.css"
+  },
+  {
+    "revision": "9582ccf7954525aff77d",
+    "url": "/microapp-edge-trials/static/js/2.491c7df7.chunk.js"
+  },
+  {
+    "revision": "455aff0743f0b953176b",
+    "url": "/microapp-edge-trials/static/js/main.b7e79e33.chunk.js"
+  },
+  {
+    "revision": "0f9eff179bc6798441b3",
+    "url": "/microapp-edge-trials/static/js/runtime~main.7e403bd5.js"
+  }
+]);
