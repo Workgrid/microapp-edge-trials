0adcf00da2096339abbb13a5d71e6193ab06ce53
Chris Weed <chris@workgrid.com>
2019-08-13 11:16:22 -0500

    🔥

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Summary from './pages/Summary'
-import IFrameDetail from './pages/IFrameDetail'
+// import IFrameDetail from './pages/IFrameDetail'
 // import GeolocationDetail from './pages/GeolocationDetail'
 
 const App = () => {
   return (
     <Router basename={window.location.pathname}>
       <Route path="/" exact component={Summary} />
-      <Route path="/iframe" component={IFrameDetail} />
+      {/* <Route path="/iframe" component={IFrameDetail} /> */}
       {/* <Route path="/geolocation" component={GeolocationDetail} /> */}
     </Router>
   )
