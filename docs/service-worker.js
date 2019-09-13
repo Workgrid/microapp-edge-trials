1996d095e1cf65847e1ceacc09e6aa888b2479f1
Chris Weed <chris@workgrid.com>
2019-09-13 09:27:38 -0500

    🔥

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/microapp-edge-trials/precache-manifest.e32d69f1b0be8701d47d8899b84e7488.js"
+  "/microapp-edge-trials/precache-manifest.3970bfc3d19a0614d98bcaf9bd452f3d.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,7 +1,11 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import MicroApp from '@workgrid/micro-app'
+import { decode } from 'jsonwebtoken'
+import './summary.css'
 
 const Summary = () => {
+  const [tests, setTests] = useState({ token: false })
+
   const microapp = useRef(
     new MicroApp({
       id: 'edge-microapp',
@@ -9,11 +13,14 @@ const Summary = () => {
     })
   )
 
+  /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
     console.log('Initialized')
     microapp.current.initialize()
+    testToken()
     console.log('Done initializing')
   }, [])
+  /* eslint-enable */
 
   const showDetail = detailPage => {
     // We have to use the hash router to show detail due to Github pages limitations
@@ -24,12 +31,36 @@ const Summary = () => {
     })
   }
 
+  const testToken = async () => {
+    const token = await microapp.current.getToken()
+    // This would be done on a server side, notice how I'm not verifying the token
+    const decodedToken = decode(token)
+    if (decodedToken.aud === process.env.REACT_APP_AUD) {
+      setTests({ ...tests, token: true })
+    } else {
+      console.warn(`Invalid token: ${decodedToken.aud} !== ${process.env.REACT_APP_AUD}`)
+    }
+  }
+
   return (
     <div>
       <h1>Edge Microapp</h1>
       <p>This is the "edgy" microapp.</p>
       <section className="section">
         <input type="text" placeholder="Input Test"></input>
+        <strong>Tests:</strong>
+        <div className="checklist">
+          {tests.token ? (
+            <span aria-label="pass" role="img" className="pass">
+              ✅
+            </span>
+          ) : (
+            <span aria-label="fail" role="img" className="pass">
+              ❌
+            </span>
+          )}
+          <div className="item">Token</div>
+        </div>
       </section>
       <div className="action-block vertical">
         <button className="secondary" onClick={() => showDetail('iframe')}>
