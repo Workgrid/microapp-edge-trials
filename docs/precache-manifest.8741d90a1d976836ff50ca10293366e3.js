9bb04fdbf527ec962da4d4046a7fbccc6ee92fc5
Chris Weed <chris@workgrid.com>
2019-09-19 05:17:19 -0500

    🔥

--- docs/precache-manifest.8741d90a1d976836ff50ca10293366e3.js
+++ docs/precache-manifest.8741d90a1d976836ff50ca10293366e3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a4a890d08f4b6e57ad247d9ccfdd21d",
+    "revision": "e37452f45f40fcf5e7671086aaf27d0c",
     "url": "/microapp-edge-trials/index.html"
   },
   {
-    "revision": "fcba04e9070101d1d27d",
+    "revision": "cfafc6730ac2086b674c",
     "url": "/microapp-edge-trials/static/css/main.8deef625.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/microapp-edge-trials/static/js/2.11805acb.chunk.js"
   },
   {
-    "revision": "fcba04e9070101d1d27d",
-    "url": "/microapp-edge-trials/static/js/main.63ed53f0.chunk.js"
+    "revision": "cfafc6730ac2086b674c",
+    "url": "/microapp-edge-trials/static/js/main.9b7bfbf7.chunk.js"
   },
   {
     "revision": "76488209e71480c79a23",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/microapp-edge-trials/precache-manifest.be8b48cc7ced13cd3f23a0ea92e78b50.js"
+  "/microapp-edge-trials/precache-manifest.8741d90a1d976836ff50ca10293366e3.js"
 );
 
 self.addEventListener('message', (event) => {
