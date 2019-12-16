a68f198b30469acc510b34aa664f7608392868ca
n0148094 <Christopher.Weed@workgrid.com>
2019-12-16 17:58:21 -0600

    Some theme trials

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'core-js/stable'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import './styles/theme.scss'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
