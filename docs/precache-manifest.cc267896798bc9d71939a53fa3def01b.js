6ebbc22783fabd86e49559d4ddfed03cfd26af2c
n0148094 <Christopher.Weed@workgrid.com>
2020-04-20 13:36:14 -0500

    New stuff

---
+++ docs/precache-manifest.cc267896798bc9d71939a53fa3def01b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f6b6d83388576ef98a6d03b0307fb4",
+    "url": "/microapp-edge-trials/index.html"
+  },
+  {
+    "revision": "dfdbb307f1816fd293cb",
+    "url": "/microapp-edge-trials/static/css/main.999ac67e.chunk.css"
+  },
+  {
+    "revision": "f490ecc3e4e3f1e940ef",
+    "url": "/microapp-edge-trials/static/js/2.2c151c5a.chunk.js"
+  },
+  {
+    "revision": "2c0c89043d5de78824a6c0ebd1cde3a7",
+    "url": "/microapp-edge-trials/static/js/2.2c151c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdbb307f1816fd293cb",
+    "url": "/microapp-edge-trials/static/js/main.c9f0eead.chunk.js"
+  },
+  {
+    "revision": "e401dde5d75e79623f24",
+    "url": "/microapp-edge-trials/static/js/runtime-main.911ff17f.js"
+  }
+]);
